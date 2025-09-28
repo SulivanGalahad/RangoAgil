51dc878dc58f654ba316bfa855600e21902f4a43
SulivanGalahad <sulivanpm@gmail.com>
2025-09-28 01:34:22 -0300

    Add EFCore

---
+++ RangoAgil.API/Entities/Rango.cs
@@ -0,0 +1,18 @@
+﻿namespace RangoAgil.API.Entities;
+
+public class Rango
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+
+    public Rango()
+    {
+        
+    }
+
+    public Rango(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
+}
