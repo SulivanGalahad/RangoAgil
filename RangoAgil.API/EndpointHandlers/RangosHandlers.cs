0a8e80d92e1f599fdcdfd8877e769f8f7e18f615
SulivanGalahad <sulivanpm@gmail.com>
2025-10-26 22:45:37 -0300

    Logs

--- RangoAgil.API/EndpointHandlers/RangosHandlers.cs
+++ RangoAgil.API/EndpointHandlers/RangosHandlers.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using RangoAgil.API.DbContexts;
 using RangoAgil.API.Entities;
 using RangoAgil.API.Models;
@@ -13,6 +14,7 @@ public static class RangosHandlers
    public static async Task<Results<NoContent, Ok<IEnumerable<RangoDTO>>>> GetRangosAsync
         (RangoDbContext rangoDbContext,
         IMapper mapper,
+        ILogger<RangoDTO> logger,
         [FromQuery(Name = "name")] string? rangoNome) 
         {
             var rangosEntity = await rangoDbContext.Rangos
@@ -21,10 +23,12 @@ public static class RangosHandlers
 
             if (rangosEntity.Count <= 0 || rangosEntity == null)
             {
+                logger.LogInformation($"GetRangosAsync registro não encontrado. Parametro: {rangoNome}");
                 return TypedResults.NoContent();
             }
             else
             {
+                logger.LogInformation("GetRangosAsync executado com sucesso!");
                 return TypedResults.Ok(mapper.Map<IEnumerable<RangoDTO>>(rangosEntity));
             }
         }
@@ -33,16 +37,19 @@ public static class RangosHandlers
     public static async Task<Results<NoContent, Ok<RangoDTO>>> GetRangosWithIdAsync
         (RangoDbContext rangoDbContext,
         IMapper mapper,
+        ILogger<RangoDTO> logger,
         int rangoId)
         {
             var rangosEntity = await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == rangoId);
 
             if (rangosEntity == null) 
-            { 
+            {
+                logger.LogInformation($"GetRangosWithIdAsync registro não encontrado. Parametro: {rangoId}");
                 return TypedResults.NoContent(); 
             }
             else
             {
+                logger.LogInformation("GetRangosWithIdAsync executado com sucesso!");
                 return TypedResults.Ok(mapper.Map<RangoDTO>(rangosEntity));
             }
             
@@ -51,6 +58,7 @@ public static class RangosHandlers
     public static async Task<CreatedAtRoute<RangoDTO>> PostRangosAsync
         (RangoDbContext rangoDbContext,
         IMapper mapper,
+         ILogger<RangoDTO> logger,
         [FromBody] RangoForCreateDTO rangoForCreate)
         {
             var rangoEntity = mapper.Map<Rango>(rangoForCreate);
@@ -58,6 +66,8 @@ public static class RangosHandlers
             await rangoDbContext.SaveChangesAsync();
 
             var rangoToReturn = mapper.Map<RangoDTO>(rangoEntity);
+
+            logger.LogInformation($"PostRangosAsync executado com sucesso! Id: {rangoToReturn.Id}");
 
             return TypedResults.CreatedAtRoute(
                 rangoToReturn,
@@ -69,6 +79,7 @@ public static class RangosHandlers
     public static async Task<Results<NotFound, Ok>> PutRangosAsync
         (RangoDbContext rangoDbContext,
         IMapper mapper,
+        ILogger<RangoDTO> logger,
         int rangoId,
         [FromBody] RangoForUpdateDTO rangoForUpdateDTO)
         {
@@ -76,6 +87,7 @@ public static class RangosHandlers
 
             if (rangoEntity == null)
             {
+                logger.LogInformation($"PutRangosAsync registro não encontrado. Parametro: {rangoId}");
                 return TypedResults.NotFound();
             }
 
@@ -83,24 +95,30 @@ public static class RangosHandlers
 
             await rangoDbContext.SaveChangesAsync();
 
+            logger.LogInformation($"PutRangosAsync registro alterado com sucesso! Parametro: {rangoId}");
+
             return TypedResults.Ok();
 
         }
 
     public static async Task<Results<NotFound, NoContent>> DeleteRangosAsync
         (RangoDbContext rangoDbContext,
+         ILogger<RangoDTO> logger,
         int rangoId) 
         {
             var rangoEntity = await rangoDbContext.Rangos.FirstOrDefaultAsync(x => x.Id == rangoId);
 
             if (rangoEntity == null)
             {
+                logger.LogInformation($"DeleteRangosAsync registro não encontrado. Parametro: {rangoId}");
                 return TypedResults.NotFound();
             }
 
             rangoDbContext.Rangos.Remove(rangoEntity);
    
             await rangoDbContext.SaveChangesAsync();
+
+            logger.LogInformation($"DeleteRangosAsync registro excluído com sucesso! Parametro: {rangoId}");
 
             return TypedResults.NoContent();
 
